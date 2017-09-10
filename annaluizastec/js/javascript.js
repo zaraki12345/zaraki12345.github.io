2afeea11c318f7b30368f18325a96cc8885f2eb1
Michał <zaraski12345@gmail.com>
2017-09-10 14:59:22 +0200

    Main text

--- annaluizastec/js/javascript.js
+++ annaluizastec/js/javascript.js
@@ -14,3 +14,27 @@ function topFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
+
+
+(function ($) {
+    $(document).ready(function(){
+
+        // hide .navbar first
+        $(".navbar").hide();
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 300) {
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+
+
+        });
+
+    });
+}(jQuery));
