f3540709e38a53d1e46117449e7e14882b83aa2d
Michał <zaraski12345@gmail.com>
2017-09-12 18:13:20 +0200

    Change color text and background jpeg

--- annaluizastec/js/javascript.js
+++ annaluizastec/js/javascript.js
@@ -26,7 +26,7 @@ function topFunction() {
         $(function () {
             $(window).scroll(function () {
                 // set distance user needs to scroll before we fadeIn navbar
-                if ($(this).scrollTop() > 300) {
+                if ($(this).scrollTop() > 100) {
                     $('.navbar').fadeIn();
                 } else {
                     $('.navbar').fadeOut();
