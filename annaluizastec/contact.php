e713f84d369462e56d2168e0892d09ac41016279
Michał <zaraski12345@gmail.com>
2017-10-17 17:11:03 +0200

    Less poprawione, szybkość także. Strona wdrożona na annaluizastec.pl

--- annaluizastec/contact.php
+++ annaluizastec/contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'zaraki12345@annaluizastec.cba.pl';
+$from = 'zaraki12345@annaluizastec.pl';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'zaraki12345@annaluizastec.cba.pl';
+$sendTo = 'zaraki12345@annaluizastec.pl';
 
 // subject of the email
 $subject = 'New message from contact form';
