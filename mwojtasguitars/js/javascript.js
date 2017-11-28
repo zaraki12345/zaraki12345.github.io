cc5833a91fb8ddda59cd203f21b44f3b6a4ffd2f
Michał <zaraski12345@gmail.com>
2017-11-28 03:18:33 +0100

    First upload new site for Michał Wojtas Guitars

---
+++ mwojtasguitars/js/javascript.js
@@ -0,0 +1,71 @@
+
+// BUTTON SCROLL TO TOP SHOW UP AFTER 250PX
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+$(function() {
+        $(".scroll, .navbar-brand").click(function(){
+            $("html,body").animate({
+                scrollTop:$(".thetop").offset().top},"1000");
+            return false})
+    }
+)
+
+/* NAVBAR SHOW UP SKRYPT */
+
+$(document).ready(function(){
+    $(window).bind('scroll', function() {
+        var navHeight = $( window ).height() - 70;
+        if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+        }
+        else {
+            $('nav').removeClass('fixed');
+        }
+    });
+});
+
+
+
+//SMOOTH SCROLLING SKRYPT:
+
+// Add smooth scrolling on all links inside the navbar
+$('#main-navbar a, header a').on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+
+    } // End if
+
+});
+
+// AOS ANIMACJA
+
+AOS.init({
+    duration: 1200,
+})
+
+
+
