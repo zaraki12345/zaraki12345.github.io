d243b19d04aa3ed6d75829d69044406d4c30e830
Michał <zaraski12345@gmail.com>
2017-12-06 18:38:39 +0100

    Upload images, other content

--- mwojtasguitars/contact.php
+++ mwojtasguitars/contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'zaraki12345@annaluizastec.pl';
+$from = 'michal.wojtas89@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'zaraki12345@annaluizastec.pl';
+$sendTo = 'michal.wojtas89@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
